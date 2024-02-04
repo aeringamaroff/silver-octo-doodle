72be63c21c2bd8af3e042af405196b5575f87bc1
Aerin Gamaroff <aerin@centbee.com>
2024-02-04 14:38:41 +0200

    Added files

--- src/index.js
+++ src/index.js
@@ -1,43 +1,70 @@
-import { Client, IntentsBitField, ActivityType } from "discord.js";
-import "dotenv/config";
+/*
+	Author: Ramzi Sah#2992
+	Fork by: FileEditor97
+	Desription:
+		creates multiple instances of the bot
+*/
+//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------
+// read configs
+const fs = require("fs");
+if (!fs.existsSync(__dirname + "/config.json")) {
+  console.warn(
+    "Config file not found! Check README.md for config.json file and place it in '" +
+      __dirname +
+      "' folder."
+  );
+  process.exit(0);
+}
+const config = JSON.parse(fs.readFileSync(__dirname + "/config.json", "utf8"));
 
-const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
+// create temp data folders
+if (!fs.existsSync(__dirname + "/temp")) {
+  fs.mkdirSync(__dirname + "/temp");
+}
+if (!fs.existsSync(__dirname + "/temp/graphs")) {
+  fs.mkdirSync(__dirname + "/temp/graphs");
+}
+if (!fs.existsSync(__dirname + "/temp/data")) {
+  fs.mkdirSync(__dirname + "/temp/data");
+}
+
+//---------------------------------------------------------------------------------------------------
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+function getTime() {
+  return new Date().toLocaleString("en-GB", timeZone).replace(/,/, "");
+}
+
+// resolve discord.com
+require("dns").resolve("www.discord.com", function (err) {
+  if (err) {
+    console.log("No connection to Discord");
+    process.exit(1);
+  } else {
+    console.log("Connected to Discord");
+  }
 });
 
-client.on("ready", (c) => {
-  console.log(`${c.user.username} IS ONLINE`);
-});
+// initiation
+const ChildProcess = require("child_process");
+var instances = [];
 
-client.on("interactionCreate", (interaction) => {
-  if (!interaction.isChatInputCommand) return;
+// start instances
+for (let i = 0; i < config["instances"].length; i++) {
+  // create child process for every instance
+  let instance = ChildProcess.fork(__dirname + "/bot.js");
 
-  if (interaction.commandName === "respond-test") {
-    interaction.reply("Responding to the command!");
-  }
-
-  if (interaction.commandName === "get-status") {
-    interaction.reply("Fetching server data!");
-  }
-
-  if (interaction.commandName === "connect") {
-    const ip = interaction.options.get("ip-address");
-
-    // TODO: validate IP address
-
-    interaction.reply(`Connecting to ${ip?.value} ...`);
-  }
-});
-
-client.on("ready", (c) => {
-  client.user.setActivity({
-    name: "Server",
-    type: ActivityType.Listening,
+  instance.on("message", (m) => {
+    if (m.error) {
+      console.error("[%s][%s]: %s\n%s", getTime(), m.id, m.message, m.error);
+    } else {
+      console.log("[%s][%s]: %s", getTime(), m.id, m.message);
+    }
   });
-});
 
-client.login(process.env.TOKEN);
+  // communicate id to instance
+  instance.send({ id: i });
+
+  // push to instances list
+  instances.push(instance);
+}
